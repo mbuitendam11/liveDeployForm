c9217dce2febc94dcba933608071f8a26d67e19e
Mathew Buitendam <mathew.buitendam11@gmail.com>
2024-03-27 15:06:42 +1100

    working on archive-delete. Finding issues with implementation

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class FormInfo(db.Model):
     firstName: Mapped[str] = mapped_column(String(250), nullable=False)
     lastName: Mapped[str] = mapped_column(String(250), nullable=False)
     text: Mapped[str] = mapped_column(Text, nullable=False) 
+    isArchived: Mapped[bool] = mapped_column(String, nullable=False)
 
 with app.app_context():
     db.create_all()
@@ -41,7 +42,8 @@ def index():
         new_record = FormInfo(
             firstName = form.firstName.data,
             lastName = form.lastName.data,
-            text = form.content.data
+            text = form.content.data, 
+            isArchived = False 
         )
 
         db.session.add(new_record)
@@ -54,9 +56,16 @@ def index():
 def this_worked():
     result = db.session.execute(
         db.select(FormInfo)
+        .where(FormInfo.isArchived == False)
     )
     all_results = result.scalars().all()
-    return render_template("this_worked.html", queries = all_results)
+
+    archived = db.session.execute(
+        db.select(FormInfo)
+        .where(FormInfo.isArchived == True)
+    )
+    archived_results = archived.scalars().all()
+    return render_template("this_worked.html", queries = all_results, archived_results=archived_results)
 
 # Update
 @app.route("/update/<int:id>", methods=["GET", "POST"])
@@ -78,11 +87,31 @@ def patch(id):
         return render_template("update-form.html", form=form, item_to_update=item_to_update, id=id)
 
 
-
 # ARCHIVE
+app.route("/archive/<int:id>", methods=["GET", "POST"])
+def archive(id):
+    item_to_archive = FormInfo.query.get_or_404(id)
+
+    if request.method == "POST":
+        item_to_archive.isArchived = True
+        try:
+            db.session.commit()
+            return redirect(url_for("this_worked"))
+        except:
+            flash("Something went wrong!")
+            return render_template("this_worked.html")
+    else:
+        return render_template("this_worked.html")
+
 
 # DELETE
-
+'''app.route("/delete/<archive_id>", methods=["POST"])
+def delete(archive_id):
+    item = FormInfo.query.get_or_404(archive_id)
+    db.session.delete(item)
+    db.session.commit()
+    flash("Item was deleted")
+    return redirect(url_for("this_worked"))'''
 
 ## Debugger ##
 
